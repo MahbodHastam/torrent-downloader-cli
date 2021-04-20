22c6a71c99c7b40de613c44f9e844aeae05b374d
Mahbod <mahbod4782@gmail.com>
2021-04-20 23:09:46 +0430

    update package.json

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-#!/bin/node
+#!/usr/bin/env node
 
-const WebTorrent = require('webtorrent-hybrid');
-const fs = require('fs');
-const os = require('os');
+const WebTorrent = require("webtorrent-hybrid");
+const fs = require("fs");
+const os = require("os");
 const hash = process.argv[2];
-const client = new WebTorrent;
-const cliProgress = require('cli-progress');
+const client = new WebTorrent();
+const cliProgress = require("cli-progress");
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.rect);
 
 if (!hash) {
-  console.log('You must enter your torrent link as an argument');
-  console.log('./torrent-downloader-cli LINK');
+  console.log("You must enter your torrent link as an argument");
+  console.log("./torrent-downloader-cli LINK");
   process.exit();
 }
 
 const getDownloadsDirectory = () => {
   // return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
-  return os.homedir() + '/Downloads';
+  return os.homedir() + "/Downloads";
 };
 
-
 try {
-  client.add(hash, torrent => {
+  client.add(hash, (torrent) => {
     const files = torrent.files;
     let length = files.length;
     console.log(`Files Count: ${length}`);
-    console.log(`Downloading in your Downloads directory: "${getDownloadsDirectory()}"`);
+    console.log(
+      `Downloading in your Downloads directory: "${getDownloadsDirectory()}"`
+    );
     bar.start(100, 0);
     let interval = setInterval(() => {
       bar.update(torrent.progress * 100);
     }, 1000);
-    files.forEach(file => {
+    files.forEach((file) => {
       const source = file.createReadStream();
-      const dest = fs.createWriteStream(`${getDownloadsDirectory()}/${file.name}`);
-      source.on('end', () => {
-        console.log(`\n----\nfile "${file.name}" downloaded.\n`);
-        length -= 1;
-        if (!length) {
-          bar.stop();
-          clearInterval(interval);
-          process.exit();
-        }
-      }).pipe(dest);
+      const dest = fs.createWriteStream(
+        `${getDownloadsDirectory()}/${file.name}`
+      );
+      source
+        .on("end", () => {
+          console.log(`\n----\nfile "${file.name}" downloaded.\n`);
+          length -= 1;
+          if (!length) {
+            bar.stop();
+            clearInterval(interval);
+            process.exit();
+          }
+        })
+        .pipe(dest);
     });
   });
 } catch (err) {
